bce6b0bcabceed5cb7accce5fbd1a6d60288b985
VictorMiyagaki <111473755+VictorMiyagaki@users.noreply.github.com>
2023-03-28 15:35:47 -0300

    Fixed localhost

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "http://127.0.0.1:3000/auth/google/secrets"
 },
     function (accessToken, refreshToken, profile, cb) {
         console.log(profile);
